fd7daca502c9b4c9f356a9004e04b0f467a2affe
sophie Haasnoot <ms.haasnoot@gmail.com>
2022-11-28 17:27:04 +0100

    added navbar, carousel and cards to homepage

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module RentAPet
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.paths << Rails.root.join("app", "assets", "images", "levers")
   end
+
+
 end
