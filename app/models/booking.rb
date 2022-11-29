fb97204fc8b27d68b296df0a76d71329d54b188c
Dmytro Plechun <ifunnd@gmail.com>
2022-11-29 18:59:30 +0200

    fixes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,12 +1,12 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :pet
+
   validates :user_id, presence: true
   validates :pet_id, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates :total_price, presence: true
 
+  validates :start_date, :end_date, presence: true
+  validates :total_price, presence: true
 
   def calculate_total_price
     (end_date - start_date).to_i * pet.price
