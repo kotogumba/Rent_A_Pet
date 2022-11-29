6090afcd2119a02cc6d3e4b5c7fa3d97889af13e
Dmytro Plechun <ifunnd@gmail.com>
2022-11-29 15:39:34 +0200

    bookings model

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :pets
+  resources :pets do
+    resources :bookings, only: [:create]
+  end
+  resources :bookings, only: [:show, :new, :update, :destroy]
 end
